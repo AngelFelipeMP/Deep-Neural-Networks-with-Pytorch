61ced9d78eba500d50ece196b1e003901fe59f50
AngelFelipeMP <angel.magnossao@gmail.com>
2023-03-08 17:04:01 +0100

    finish Gradiant Descent script

--- Week2/Gradiant_descent_pytorch.py
+++ Week2/Gradiant_descent_pytorch.py
@@ -3,13 +3,45 @@ import matplotlib.pyplot as plt
 
 w = torch.tensor(10.0, requires_grad=True)
 X = torch.arange(-3,3,0.1).view(-1,1)
-f = -3*X
+f = -3*X  #line equation b=0
+Y = f + 0.5* torch.randn(X.size())
 
-# print(X)
-# print(X.size())
-# print(X.ndim)
-# print(X.shape)
 
 plt.plot(X.numpy(),f.numpy())
+plt.plot(X.numpy(),Y.numpy(),'ro')
+plt.xlabel("X")
+plt.ylabel("Y")
 plt.show()
 
+
+def forward(x):
+  return w*x
+
+
+def criterion(yhat,y):
+  return torch.mean((yhat-y)**2)
+
+
+lr=0.1
+cost=[]
+epochs=[]
+for epoch in range(4):
+  Yhat = forward(X)
+  loss = criterion(Yhat, Y)
+  loss.backward()
+  w.data = w.data - lr * w.grad.data
+  w.grad.data.zero_()
+  cost.append(loss.item())
+  epochs.append(epoch)
+
+  plt.plot(X.detach().numpy(),Yhat.detach().numpy(), label="line"+str(epoch))
+leg = plt.legend(loc='upper center')
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
+# Loss plot
+plt.plot(epochs,cost)
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.show()
